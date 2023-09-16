588b6ea021b6fd3dfdb94e435fcc88e9a1b023df
Thiago Bignotto <bignotto@gmail.com>
2023-09-16 07:54:51 -0300

    added and configured clerk auth provider

--- App.tsx
+++ App.tsx
@@ -1,10 +1,7 @@
+import { ClerkProvider } from "@clerk/clerk-expo";
 import { Inter_900Black, useFonts } from "@expo-google-fonts/inter";
 import { GluestackUIProvider } from "@gluestack-ui/themed";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "@screens/Home";
-
-const Stack = createNativeStackNavigator();
+import Routes from "@routes/index";
 
 export default function App() {
   const CLERK_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
@@ -18,12 +15,10 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <GluestackUIProvider>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </GluestackUIProvider>
-    </NavigationContainer>
+    <GluestackUIProvider>
+      <ClerkProvider publishableKey={`${CLERK_KEY}`}>
+        <Routes />
+      </ClerkProvider>
+    </GluestackUIProvider>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,30 @@
-import { Box, Text } from "@gluestack-ui/themed";
+import { useAuth, useOAuth } from "@clerk/clerk-expo";
+import { Box, Button, Divider, Text } from "@gluestack-ui/themed";
+import { useWarmUpBrowser } from "@hooks/warmUpBrowser";
+import { useCallback } from "react";
 
 export default function Home() {
+  const { userId, signOut } = useAuth();
+
+  useWarmUpBrowser();
+
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const handleGoogle = useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+
   return (
     <Box
       width="100%"
@@ -10,6 +34,19 @@ export default function Home() {
       backgroundColor="$green300"
     >
       <Text>This is version 0</Text>
+      <Text>User id is {userId}</Text>
+
+      <Button onPress={handleGoogle}>
+        <Text color="$white" fontWeight="$bold">
+          Log in with google
+        </Text>
+      </Button>
+      <Divider my="$0.5" />
+      <Button onPress={() => signOut()}>
+        <Text color="$white" fontWeight="$bold">
+          LOG OUT
+        </Text>
+      </Button>
     </Box>
   );
 }
